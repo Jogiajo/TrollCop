b0cbc9fdd97ebbff0009fc2bae0e66188bb82a21
MahathiVavilala <mahathivavilala97@gmail.com>
2018-01-25 02:36:47 +0530

    Modified SentimentalAnalysis.py

--- SentimentalAnalysis.py
+++ SentimentalAnalysis.py
@@ -1,10 +1,32 @@
-def clean_tweet(tweet):
-    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-def analize_sentiment(tweet):
-    analysis = TextBlob(clean_tweet(tweet))
+import pandas as pd
+import os
+import re
+
+def readWordList():
+    cwd = os.getcwd()
+    data = open(cwd + "/BadWordsList.txt", "rb").read()
+    data = "".join(map(chr, data))
+    return data
+
+def cleanData(data) :
+   return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", data).split())
+
+def offensive(tweet):
+    #msg = "Tweet stuff here"
+    data = cleanData(readWordList())
+    data = data.split(" ")
+    for i in data:
+        if tweet.find(i) == -1:
+            continue
+        else :
+            print("Offensive")
+            break
+            
+def sentiment(tweet):
+    analysis = TextBlob(cleanData(tweet))
     if analysis.sentiment.polarity > 0:
         return 1
     elif analysis.sentiment.polarity == 0:
         return 0
     else:
-        return -1
+        return offensive(tweet)
