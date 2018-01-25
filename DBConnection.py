835d28642b48ef57602f4c425939a4c20635b803
Rajula Pavani <rpavani1998@gmail.com>
2018-01-25 12:01:45 +0530

    Added Database Operations

--- DBConnection.py
+++ DBConnection.py
@@ -2,26 +2,70 @@ import pymysql.cursors
 
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='pingu',
+                             password='root',
                              db='TrollCop',
-                             charset='utf8mb4',
+                             charset='utf8',
                              cursorclass=pymysql.cursors.DictCursor)
 
-def insert_into_table():
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("insert into user values('webmaster@python.org', 10)")
-            connection.commit()
-    finally:
-        connection.close()
+def insertUser(userName, spamCount = 0):
+    with connection.cursor() as cursor:
+        print("InsertUser",userName)
+        cursor.execute("insert into `Users` values( %s, %s)",(userName, spamCount))
+        connection.commit()
+    
 
-def retrieve_from_table(): 
-    try:
-        with connection.cursor() as cursor:
-            sql = "SELECT * FROM `User`"
-            cursor.execute(sql)
-            result = cursor.fetchall()
-            for i in result:
-                print(i)
-    finally:
-        connection.close()
+
+def isExistingUser(userName):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `Users` where userName = '%s'" % (userName)
+        print(sql)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            
+            return True
+    
+    return False
+
+def updateUser(userName):
+    with connection.cursor() as cursor:
+        sql = "SELECT spamCount FROM `Users` where userName = '%s'" % (userName)
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        spamCount = result.get('spamCount')
+        spamCount += 1
+        sql = "Update `Users` set spamCount = %s where userName = '%s'" % (spamCount,userName)
+        print(sql)
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            connection.commit()
+            return True
+            
+ 
+    return False
+
+
+def retrieveUsers(): 
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `Users`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            print(i)
+   
+
+def insertTweet(tweetId, userName, tweet, timestamp, isTroll):
+    with connection.cursor() as cursor:
+        cursor.execute("insert into `Tweets` values( %s, %s, %s, %s, %s)",(tweetId, userName, tweet, timestamp, int(isTroll)))
+        connection.commit()
+ 
+
+def retrieveUsers(): 
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM `Tweets`"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        for i in result:
+            print(i)
+   
